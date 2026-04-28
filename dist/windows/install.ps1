b75f2c10d53ecb38ee2914f470ae9557667d92e9
Ryan Wesley Amoroso <rwamoroso@gmail.com>
2026-04-28 16:16:07 -0500

    pcsigning

--- dist/windows/install.ps1
+++ dist/windows/install.ps1
@@ -4,5 +4,10 @@ $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 $certificatePath = Join-Path $scriptDir 'CEOCommunicationTrainer.cer'
 $packagePath = Join-Path $scriptDir 'CEOCommunicationTrainer.msix'
 
+Import-Module Microsoft.PowerShell.Security -ErrorAction Stop
+if (-not (Get-PSDrive -Name Cert -ErrorAction SilentlyContinue)) {
+    New-PSDrive -Name Cert -PSProvider Certificate -Root '\' | Out-Null
+}
+
 Import-Certificate -FilePath $certificatePath -CertStoreLocation 'Cert:\CurrentUser\Root' | Out-Null
 Add-AppxPackage -Path $packagePath
